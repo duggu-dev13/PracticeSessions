3487cb48f15732ced1531c81dd3213288b6a39d1
Duggu@1308 <work.vaibhav1308@gmail.com>
2025-10-19 05:40:58 +0000

    Optimising the app

--- week_6/week_6_experiments/src/App.jsx
+++ week_6/week_6_experiments/src/App.jsx
@@ -2,21 +2,28 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [title, setTitle] = useState("My Title")  
-
-  function updateTitle(){
-    setTitle("New title is: " + Math.random());
-  }
-
   return (
     <>
-      <button onClick={updateTitle}>Click me to change the Header</button>
-      <Header title={title} />      
+      <HeaderWithButton />
       <Header title="My Title" />      
     </>
   )
 }
 
+function HeaderWithButton() {
+  const [title, setTitle] = useState("My Title")  
+
+  function updateTitle(){
+    setTitle("New title is: " + Math.random());
+  }
+  
+  return (
+    <>
+      <button onClick={updateTitle}>Click me to change the Header</button>
+      <Header title={title} />      
+    </>  
+  )
+}
 
 function Header({title}) {
   return <div>

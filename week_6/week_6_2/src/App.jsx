ea9eeab7305b2a9629825a31920be954302b0e4d
Duggu@1308 <work.vaibhav1308@gmail.com>
2025-10-20 15:25:20 +0000

    customHooks

--- week_6/week_6_2/src/App.jsx
+++ week_6/week_6_2/src/App.jsx
@@ -1,30 +1,27 @@
-import { memo, useCallback, useState } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count ,setCount] = useState(0);
-  
-  // useCallback will return a memoized version of the callback that only changes if one of the dependencies has changed. or in simple way it will remember the function reference  
-  const inputFunction = useCallback(() => {
-    console.log("Button Clicked");
+function useTodo() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/todos').then((response) => {
+      setTodos(response.data);
+    });
   }, [])
 
+  return todos;
+}
+
+
+function App() {
+const todos = useTodo();
   return (
     <>
-      <ButtonComponent inputFunction={inputFunction}></ButtonComponent> <br />
-      <button onClick={() => {
-        setCount(count + 1);
-      }}>Counter {count}</button>
+    {todos}
     </>
   )
 }
 
-const ButtonComponent = memo(({inputFunction}) => {
-  console.log("Child rendered");
-
-  return (
-    <button onClick={inputFunction}>Click Me</button>
-  )
-})
 
 export default App

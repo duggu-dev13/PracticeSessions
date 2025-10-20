c1cd37b4488689437fd2af994110d2aa638585a8
Duggu@1308 <work.vaibhav1308@gmail.com>
2025-10-20 09:03:35 +0000

    Practice Sessions

--- week_6/week_6_experiments/src/App.jsx
+++ week_6/week_6_experiments/src/App.jsx
@@ -1,58 +1,18 @@
-import { useState, memo } from 'react';
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    alert('Hello World!')}
+    , [])
+
   return (
     <>
-      <CardWrappper>
-        <Texts />
-        <Texts />
-      </CardWrappper>
-
-      <CardWrappper>
-        <Texts />
-        <Texts />
-        <Texts />
-        <Texts />
-      </CardWrappper>
-
-      {/* <CardWrappper innerCommpoenent={<TextComponent1 />} /> */}
+      <div>
+        hi there
+      </div>
     </>
   )
 }
-
-function CardWrappper({children}){
-  console.log(children)
-  return (
-    <div style={{border: '1px solid black', padding: '20px', margin: '20px'}}>
-      {children}
-    </div>
-  )
-}
-
-function Texts(){
-  return (
-    <>
-      <h3>Hii there</h3>
-    </>
-  )
-}
-
-// function TextComponent() {
-//   return (
-//     <>
-//       <h1>hi there</h1>
-//     </>
-//   )
-// }
-
-// function TextComponent1() {
-//   return (
-//     <>
-//       <h1>hi there hi therererere</h1>
-//     </>
-//   )
-// }
-
 
 export default App

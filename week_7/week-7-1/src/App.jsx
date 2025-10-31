68011bf91869f4a6c23b63a8aa4affab37387c62
Duggu@1308 <work.vaibhav1308@gmail.com>
2025-10-31 14:28:33 +0000

    Lazy loading and Default Exports

--- week_7/week-7-1/src/App.jsx
+++ week_7/week-7-1/src/App.jsx
@@ -1,15 +1,14 @@
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'  
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom'  ;
+import Landing from './components/Landing';
+import './App.css';
 
-import './App.css'
+const Dashboard = React.lazy(() => import('./components/Dashboard'));
+// lazy loading will load the component only when it is needed. This is small way to optimisation in react websites.
 
 function App() {
   
   return (
     <>
-      
-      
       <BrowserRouter>
        <AppBar />
         <Routes>
@@ -24,7 +23,7 @@ function App() {
 function AppBar() {
 
   const navigate = useNavigate();
-  
+
   return (
     <div>
         <button onClick={() => {
--- week_7/week-7-1/src/components/Dashboard.jsx
+++ week_7/week-7-1/src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
-export function Dashboard() {
+// Defualt mean to export a single value from a module
+export default function Dashboard() {
     return (
         <>
             <h1>Dashboard Page</h1>
